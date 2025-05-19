ebebe361c146cc833ab9488e386a52f1558a819e
LotharBezzon <lothar.bezzon@gmail.com>
2025-05-19 15:49:25 +0200

    write read_data

--- read_data.py
+++ read_data.py
@@ -1,51 +1,63 @@
 import numpy as np
 import os
-import json
+import pickle
 
-def get_data(protein, num_models):
-
-    folder = f'fold_{protein}'
-
-    cpms = []
-    paes = []
-    plddtss = []
-    for i in range(num_models):
-        json_file = os.path.join(folder, f'fold_{protein}_full_data_{i}.json')
-        with open(json_file, 'r') as f:
-            data = json.load(f)
-        
-        cpms.append(np.array(data['contact_probs']))
-        paes.append(np.array(data['pae']))
-        plddtss.append(np.array(data['atom_plddts']))
-    
-    cpm = np.mean(cpms, axis=0)
-    pae = np.mean(paes, axis=0)
-    plddts = np.mean(plddtss, axis=0)
-
-    return cpm, pae, plddts
-
-def get_pos(protein):
-
-    folder = f'fold_{protein}'
-    
-    json_file = os.path.join(folder, f'fold_{protein}_full_data_0.json')
-    with open(json_file, 'r') as f:
-        data = json.load(f)
-    
-    pos = np.array(data['positions'])
-    return pos
+def check_folder_exists(protein):
+    """
+    Check if the file exists in the current directory.
+    """
+    return os.path.isfile(f'{protein}/pkl0.pickle')
 
 def read_data(protein, num_models=5):
 
-    folder = f'fold_{protein}'
-    if not os.path.exists(folder):
-        raise FileNotFoundError(f"Folder {folder} does not exist. Download data from https://alphafoldserver.com/")
-    
-    cpm, pae, plddts = get_data(protein, num_models)
+    if not check_folder_exists(protein):
+        raise FileNotFoundError(
+            f"Folder {protein}/ doesn't exist or doesn't contain necessary information.\n"
+            "Please, rename it correctly or download data from https://colab.research.google.com/github/sokrypton/ColabFold/blob/main/AlphaFold2.ipynb"
+        )
 
-    return cpm, pae, plddts
-    
-    
+    paes = []
+    dgrams = []
+
+    for i in range(num_models):
+
+        with open(f'{protein}/pkl{i}.pickle', 'rb') as f:
+
+            data = pickle.load(f)
+
+            # Extract pae
+            paes.append(np.array(data['predicted_aligned_error']))
+
+            # Extract dgram
+            dgrams.append(np.array(data['distogram']['logits']))
+
+            if i == 0:
+                # Extract bin edges
+                bins = data['distogram']['bin_edges']
+
+    pae = np.mean(paes, axis=0)
+    dgram = np.mean(dgrams, axis=0)
+
+    # Get starting positions
+    with open(f'{protein}/starting_structure.pdb', 'r') as f:
+
+        lines = f.readlines()
+        coords = []
+
+        for line in lines:
+            words = line.split()
+            if words[0] == 'ATOM':
+
+                if words[3] == 'GLY' and words[2] == 'CA':
+                    coords.append([float(words[6]), float(words[7]), float(words[8])])
+
+                if words[2] == 'CB':
+                    coords.append([float(words[6]), float(words[7]), float(words[8])])
+
+    coords = np.array(coords)
+
+    return coords, bins, pae, dgram
+
 
 
 if __name__ == "__main__":
